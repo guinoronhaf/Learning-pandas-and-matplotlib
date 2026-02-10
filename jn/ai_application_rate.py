818723c054b70c2452268f0a1aaa1877cd076109
joaoneto9 <joaonetocg1@gmail.com>
2026-02-10 16:20:22 -0300

    feat: add the ai trust script to plot the graph

--- jn/ai_application_rate.py
+++ jn/ai_application_rate.py
@@ -1,28 +1,42 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def map_times_by_rate(df_to_map):
+    times_dict = {}
+    for i in range(0, 11):
+        times_dict[i] = df_to_map.loc[df_to_map['rate'] == i, 'times_chosen'].values
+
+        if list(times_dict[i]) == []:
+            times_dict[i] = 0
+        else:
+            times_dict[i] = int(times_dict[i][0])
+
+    return times_dict
+
 
 def plot_graph(df):
     df_to_plot = get_data_frame_app_rate(df)
 
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(8, 5))
 
-    rate_label = list(df_to_plot.columns)
-    rates = list(df_to_plot["times_chosen"])
+    dict_times_by_rate = map_times_by_rate(df_to_plot)
 
-    print(rate_label)
-    print(rates)
+    rates = list(dict_times_by_rate)
+    times_chosen = [dict_times_by_rate[i] for i in rates]
 
-    bar_labels = ['rate']
-    bar_colors = ['tab:blue']
+    bar_label = 'Times Chosen'
+    bar_color = 'tab:blue'
 
-    ax.bar(rate_label, rates, label=bar_labels, color=bar_colors)
+    ax.bar(rates, times_chosen, label=bar_label, color=bar_color)
 
-    ax.set_ylabel('rate')
-    ax.set_title('rate AI trust by radiologies')
-    ax.legend('color mean')
+    ax.set_xticks(range(len(rates))) # isso permite que todos os valores (eixo-x) de rate apareçam, sem perder os dados
+    ax.set_xticklabels(rates)
+    ax.set_ylabel('Times Chosen')
+    ax.set_xlabel('Rates')
+    ax.set_title('Rate: AI trust by radiologies')
+    ax.legend(title="Color Mean")
 
-    plt.show()
+    plt.savefig("graphs/ai_trust_rated_by_radiologies.png")
 
 
 
