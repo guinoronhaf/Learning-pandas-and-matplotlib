28b59c4c8f3699442b8425eebd3935ad74ad2859
joaoneto9 <joaonetocg1@gmail.com>
2026-02-12 14:57:43 -0300

    feat: adding the ploting graphs flux into the beta script main

--- src/main.py
+++ src/main.py
@@ -1,1 +1,17 @@
-from .plot.generate_graphs import *
+from .plot import generate_graphs as g
+import pandas as pd
+
+if __name__ == "__main__":
+    CSV_PATH = "data/csv_files/mockup/respostas_questionario.csv"
+    
+    df = pd.read_csv(CSV_PATH)
+
+    print("\nGENERATING THE GRAPHS:\n")
+    g.generate_graph_1(df)
+    print("graph1 is OK")
+    g.generate_graph_2(df)
+    print("graph2 is OK")
+    g.generate_graph_3(df)
+    print("graph3 is OK")
+    g.generate_graph_6(df)
+    print("graph6 is OK")
