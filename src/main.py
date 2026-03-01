6a43b62bfa26f6943a3025fb5b504fe68dc67ec9
Guilherme Noronha <guilhermenoronhaf@gmail.com>
2026-03-01 13:13:18 -0300

    feat: adicionando funções para cálculos[ estatísticos

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
-from .plot import generate_graphs as g
 import pandas as pd
+from .plot import generate_graphs as gng
+from .stats import generate_stats as gns
 
 if __name__ == "__main__":
     CSV_PATH = "data/csv_files/mockup/respostas_questionario.csv"
@@ -7,18 +8,27 @@ if __name__ == "__main__":
     df = pd.read_csv(CSV_PATH)
 
     try:
-        print("\nGENERATING THE GRAPHS:\n")
-        g.generate_graph_1(df)
+        print("\nGENERATING GRAPHS:\n")
+        gng.generate_graph_1(df)
         print("graph1 is OK")
-        g.generate_graph_2(df)
+        gng.generate_graph_2(df)
         print("graph2 is OK")
-        g.generate_graph_3(df)
+        gng.generate_graph_3(df)
         print("graph3 is OK")
-        g.generate_graph_4(df)
+        gng.generate_graph_4(df)
         print("graph4 is OK")
-        g.generate_graph_5(df)
+        gng.generate_graph_5(df)
         print("graph5 is OK")
-        g.generate_graph_6(df)
+        gng.generate_graph_6(df)
         print("graph6 is OK")
+
+        fields = ("menos_seg", "ia_descord")
+
+        sp, psp = gns.calculate_spearmanr(df, fields)
+        kt, pkt = gns.calculate_kendallt(df, fields)
+
+        print(f"Rho: {sp:.4f}, p: {psp:.15f}")
+        print(f"Tau: {kt:.4f}, p: {pkt:.15f}")
+
     except:
         print("Something got wrong...")
