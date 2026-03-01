a47cb6c2ee1c81853a765ff3afa1e33f1bbfb1bc
Guilherme Noronha <guilhermenoronhaf@gmail.com>
2026-03-01 12:54:56 -0300

    feat: atualizando main com os cálculos estatísticos

--- guilerme/projeto_huac/src/generate_stats.py
+++ guilerme/projeto_huac/src/generate_stats.py
@@ -1,29 +1,46 @@
 import pandas as pd
 from scipy.stats import spearmanr, kendalltau
 
-df = pd.read_csv("../data/answers/mockup_respostas.csv")
-
+# campo identificador da base de dados de repostas
 ID_FIELD = "data_hora"
 
-# calculando Spearman's rho a partir de um DataFrame e dos campos contendo as colunas das variáveis que deverão ser analisadas
-def calculate_spearmanr(df: pd.DataFrame, fields: tuple) -> tuple:
-    strict_df = df[list(fields)].copy() # copia do DataFrame para evitar warning do Pandas
-    strict_df = strict_df.dropna() # removendo colunas NaN
+def calculate_spearmanr(df: pd.DataFrame, fields: tuple) -> tuple[float, float]:
+    """
+    Calcula o coeficiente de correlação de Spearman em relação a duas colunas específícas.  
 
-    mapping = dict() # inicializando dicionário
-    possible_answers = sorted(set(strict_df[fields[0]])) # sortando o set pra evitar inconsistência no mapeamento
+    Nesta função, é realizado um mapeamento das possíveis repostas para valores numéricos, o que permite o cálculo efetivo do coeficiente.
+    Além disso, são excluídos valores nulos, o que facilita o cálculo estatístico.
+
+    Parâmetros:
+        df (pd.DataFrame): DataFrame original, contendo os dados e o identificador geral.
+        fields (tuple): Tupla contendo as duas colunas a serem analisadas.
+    Retorno:
+        tuple[float, float]: Tupla contendo o coeficiente de correlação e o valor de "p" (rho, p).
+    """
+    global ID_FIELD
+
+    # Evitar Warning de Pandas
+    strict_df = df[list(fields)].copy() 
+    # Excluir valores NaN
+    strict_df = strict_df.dropna() 
+
+    mapping = dict()
+
+    # Ordenando dados (alfabeticamente) no set para evitar inconsistências no mapeamento
+    possible_answers = sorted(set(strict_df[fields[0]]))
+
     idx = 0
 
     for ans in possible_answers:
         mapping[ans] = idx
         idx += 1
 
-    # realizando mapeamento
     strict_df[fields[0]] = strict_df[fields[0]].map(mapping)
     strict_df[fields[1]] = strict_df[fields[1]].map(mapping)
 
     strict_df = strict_df.dropna()
-
+    
+    # Restaura a ordem dos índices
     strict_df = strict_df.reset_index(drop=True)
 
     sp, p = spearmanr(strict_df[fields[0]], strict_df[fields[1]])
@@ -31,24 +48,43 @@ def calculate_spearmanr(df: pd.DataFrame, fields: tuple) -> tuple:
     return (float(sp), float(p))
 
 
-def calculate_kendallt(df: pd.DataFrame, fields: tuple) -> tuple:
-    strict_df = df[list(fields)].copy() # copia do DataFrame para evitar warning do Pandas
-    strict_df = strict_df.dropna() # removendo colunas NaN
+def calculate_kendallt(df: pd.DataFrame, fields: tuple) -> tuple[float, float]:
+    """
+    Calcula o coeficiente de correlação de Kendall em relação a duas colunas específícas.  
 
-    mapping = dict() # inicializando dicionário
-    possible_answers = sorted(set(strict_df[fields[0]])) # sortando o set pra evitar inconsistência no mapeamento
+    Nesta função, é realizado um mapeamento das possíveis repostas para valores numéricos, o que permite o cálculo efetivo do coeficiente.
+    Além disso, são excluídos valores nulos, o que facilita o cálculo estatístico.
+
+    Parâmetros:
+        df (pd.DataFrame): DataFrame original, contendo os dados e o identificador geral.
+        fields (tuple): Tupla contendo as duas colunas a serem analisadas.
+    Retorno:
+        tuple[float, float]: Tupla contendo o coeficiente de correlação e o valor de "p" (tau, p).
+    """
+    global ID_FIELD
+
+    # Evitar Warning de Pandas
+    strict_df = df[list(fields)].copy() 
+    # Excluir valores NaN
+    strict_df = strict_df.dropna() 
+
+    mapping = dict()
+
+    # Ordenando dados (alfabeticamente) no set para evitar inconsistências no mapeamento
+    possible_answers = sorted(set(strict_df[fields[0]])) 
+
     idx = 0
 
     for ans in possible_answers:
         mapping[ans] = idx
         idx += 1
 
-    # realizando mapeamento
     strict_df[fields[0]] = strict_df[fields[0]].map(mapping)
     strict_df[fields[1]] = strict_df[fields[1]].map(mapping)
 
     strict_df = strict_df.dropna()
-
+    
+    # Restaura a ordem dos índices
     strict_df = strict_df.reset_index(drop=True)
 
     kt, p = kendalltau(strict_df[fields[0]], strict_df[fields[1]])
--- guilerme/projeto_huac/src/main.py
+++ guilerme/projeto_huac/src/main.py
@@ -10,12 +10,18 @@ def main_generate_graphs(df: pd.DataFrame):
     gng.generate_graph2(df)
     gng.generate_graph3(df)
 
-def main_generate_stats
+
+def main_generate_stats(df: pd.DataFrame, fields: tuple):
+    sp = gns.calculate_spearmanr(df, fields)
+    kt = gns.calculate_kendallt(df, fields)
+    print(f"Rho: {sp[0]:.4f}, p: {sp[1]:.15f}")
+    print(f"Tau: {kt[0]:.4f}, p: {kt[1]:.15f}")
 
 
 if __name__ == '__main__':
     try:
         main_generate_graphs(df)
+        main_generate_stats(df, ("mais_seg", "ia_descord"))
         print("ok!")
     except Except as e:
         print("ERRO!" + e)
