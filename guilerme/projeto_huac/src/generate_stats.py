7d1c884261cd850351d52accde78e52103b9d002
Guilherme Noronha <guilhermenoronhaf@gmail.com>
2026-03-01 12:25:22 -0300

    feat: adicionando função para cálculo do coeficiente de Spearman

---
+++ guilerme/projeto_huac/src/generate_stats.py
@@ -0,0 +1,31 @@
+import pandas as pd
+from scipy.stats import spearmanr
+
+df = pd.read_csv("../data/answers/mockup_respostas.csv")
+
+ID_FIELD = "data_hora"
+
+# calculando Spearman's rho a partir de um DataFrame e dos campos contendo as colunas das variáveis que deverão ser analisadas
+def calculate_spearmanr(df: pd.DataFrame, fields: tuple) -> tuple:
+    strict_df = df[list(fields)].copy() # copia do DataFrame para evitar warning do Pandas
+    strict_df = strict_df.dropna() # removendo colunas NaN
+
+    mapping = dict() # inicializando dicionário
+    possible_answers = sorted(set(strict_df[fields[0]])) # sortando o set pra evitar inconsistência no mapeamento
+    idx = 0
+
+    for ans in possible_answers:
+        mapping[ans] = idx
+        idx += 1
+
+    # realizando mapeamento
+    strict_df[fields[0]] = strict_df[fields[0]].map(mapping)
+    strict_df[fields[1]] = strict_df[fields[1]].map(mapping)
+
+    strict_df = strict_df.dropna()
+
+    strict_df = strict_df.reset_index(drop=True)
+
+    sp, p = spearmanr(strict_df[fields[0]], strict_df[fields[1]])
+
+    return (sp, p)
